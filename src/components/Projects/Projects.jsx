7f5926f01f698fa3fcea6a593f06e29d2db52343
Erich <48527495+aznguymp4@users.noreply.github.com>
2024-02-22 01:37:46 -0600

    Project Window
    better preview modal, mobile support

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,12 +8,21 @@ import PreviewWindow from './PreviewWindow'
 const Projects = () => {
 	const [projIdx, setProjIdx] = useState(0)
 	const [imgSel, setImgSel] = useState(null)
+	const [imgSelLoaded, setImgSelLoaded] = useState(false)
 	const focus = useSelector(s=>s.options.focus)
 	const proj = ProjectList[projIdx]
 
 	const selImg = (e, img) => {
-		setImgSel({...img, bounds: e.target.getBoundingClientRect()})
+		if(imgSel) return
+		setImgSelLoaded(false)
+		setImgSel({...img, target: e.target})
 	}
+
+	const macBtns = <>
+		<div className={`macBtn ${focus?'':'gray'}`}><i className="fa-solid fa-xmark"/></div>
+		<div className={`macBtn ${focus?'':'gray'}`}><i className="fa-solid fa-minus"/></div>
+		<div className={`macBtn ${focus?'':'gray'}`}><i className="fa-solid fa-plus"/></div>
+	</>
 
 	return <div className="projects">
 		<div className='ac s500 wbold ca700'>Projects</div>
@@ -21,11 +30,7 @@ const Projects = () => {
 		<br/>
 		<div className='projectWindow noDrag noSelect'>
 			<div className='projectWindowL'>
-				<div className={`projectWindowLTop ${focus?'':'disabled'}`}>
-					<div className={`macBtn ${focus?'':'gray'}`}><i className="fa-solid fa-xmark"/></div>
-					<div className={`macBtn ${focus?'':'gray'}`}><i className="fa-solid fa-minus"/></div>
-					<div className={`macBtn ${focus?'':'gray'}`}><i className="fa-solid fa-plus"/></div>
-				</div>
+				<div className={`projectWindowLTop ${focus?'':'disabled'}`}>{macBtns}</div>
 				<div className='projectWindowLList noScrollBar'>{
 					ProjectList.map((p,i) => <div
 						className={`projectTab ${projIdx===i?'selected':''}`}
@@ -41,12 +46,28 @@ const Projects = () => {
 				}</div>
 			</div>
 			<div className='projectWindowR'>
-				<div className='projectWindowRTop wsemibold'>{proj.name}</div>
+				<div className='projectWindowRTop wsemibold'>
+					<div className='projectWindowLTop'>
+						{macBtns}
+					</div>
+					<div>{proj.name}</div>
+					{imgSel && !imgSelLoaded && <div className='ca700 ar projectWindowRTopLoad'><i className="fa-solid fa-gear fa-spin fa-lg"/></div>}
+				</div>
+				<div className='projectWindowRTabs'>{
+					ProjectList.map((p,i) => <div
+						className={`projectTab ${projIdx===i?'selected':''}`}
+						onClick={()=>{
+							setImgSel(null)
+							setProjIdx(i)
+						}}
+						key={i}
+					>
+						<div className='projectTabIcon'><img className='' src={`./projects/${p.name}/${p.icon}`}/></div>
+						<div className='projectTabName'>{p.name}</div>
+					</div>)
+				}</div>
 				<div className='projectWindowRBody'>{
 					<>
-						<div>
-							
-						</div>
 						{proj.imgs.map((img,idx) => <div key={idx} className='projectImg'>{
 								img.vid
 								? <GifV onClick={e=>selImg(e, img)} src={`./projects/${proj.name}/${img.src}`} type={img.vid}/>
@@ -58,7 +79,13 @@ const Projects = () => {
 			</div>
 		</div>
 		
-		{imgSel && <PreviewWindow proj={proj} img={imgSel} setImgSel={setImgSel}/>}
+		{imgSel && <PreviewWindow
+			proj={proj}
+			img={imgSel}
+			setImgSel={setImgSel}
+			loaded={imgSelLoaded}
+			setLoaded={setImgSelLoaded}
+		/>}
 	</div>
 }
 
