4a6728bf9429322211c3c9850dc88a8ebf05b609
Erich <48527495+aznguymp4@users.noreply.github.com>
2024-02-23 00:33:13 -0600

    Update AboutMe.jsx

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -5,8 +5,7 @@ const AboutMe = () => {
 		<div className='s500 wbold ca700 ac'>About Me</div>
 		<div className='aboutMeBody'>
 			<div className='aboutMeDesc'>
-				Ever since I was young, I&apos;ve always had curiosity and a great understanding with technology.
-				I&apos;ve been doing web development since 2019, and am always thinking about efficiency, modularity, and minimizing repetition when writing code.
+				As a child, my creative mind had always enjoyed making things and learning about technology—that meant programming was the perfect opportunity to combine the two. I’ve been doing web development since 2019; I constantly think about efficient and modular ways to write unique code.
 			</div>
 			<i className='aboutMeDevIcon fa-solid fa-code s700 ma ct200'/>
 		</div>
