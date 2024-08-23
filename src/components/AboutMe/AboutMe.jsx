969e8d5ee140e072f6efe641ff59a0da891938e1
Erich <48527495+aznguymp4@users.noreply.github.com>
2024-08-23 13:03:45 -0500

    Update AboutMe.jsx

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -5,7 +5,7 @@ const AboutMe = () => {
 		<div className='s500 wbold ca700 ac'>About Me</div>
 		<div className='aboutMeBody'>
 			<div className='aboutMeDesc'>
-				As a child, my creative mind had always enjoyed making things and learning about technology—that meant programming was the perfect opportunity to combine the two. I’ve been doing web development since 2019; I constantly think about efficient and modular ways to write unique code.
+				Growing up, my creative mind had always enjoyed making things and learning about technology—that meant programming was the perfect opportunity to combine the two. I’ve been doing web development since 2019; I constantly think about efficient and modular ways to write unique code.
 			</div>
 			<i className='aboutMeDevIcon fa-solid fa-code s700 ma ct200'/>
 		</div>
